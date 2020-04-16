1449a007433a1ba60a868eb812b0b50c48e3b086
github username <github email address>
2020-04-16 11:26:23 +0100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "../stylesheets/App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="ui raised segment">
+        <div className="ui segment violet inverted">
+          <h2>Hello, Bryn</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
